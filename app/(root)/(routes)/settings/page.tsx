373b00a35db741f31fb3fb069956ceba075d928d
Sam Villa-Smith <43689653+samvillasmith@users.noreply.github.com>
2023-09-03 21:31:58 -0500

    fix 5

--- app/(root)/(routes)/settings/page.tsx
+++ app/(root)/(routes)/settings/page.tsx
@@ -7,7 +7,7 @@ const Settings = async () => {
         <div className="h-full p-4 space-y-2">
             <h3 className="text-lg font-medium">Settings</h3>
             <div className="text-muted-foreground text-sm">
-                { isPremium ? "You are a Premium member." : "You are currently on a afree plan."}
+                { isPremium ? "You are a Premium member." : "You are currently on a a free plan."}
             </div>
             <SubscriptionButton isPremium={isPremium}/>
         </div>
